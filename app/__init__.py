8d2b16596ef9206ce8c1286fc28e9e40453e13a3
Mikael Svensson <github@mikaelsvensson.info>
2017-01-06 16:28:27 +0100

    Parameterized host name

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,13 @@ application = Flask(__name__)
 
 bot_username = os.environ.get('KIK_BOT_USERNAME')
 bot_api_key = os.environ.get('KIK_BOT_APIKEY')
+host_name = os.environ.get('APP_HOST_NAME')
 
 kik = KikApi(bot_username, bot_api_key)
 
 kik.set_configuration(
     Configuration(
-        webhook='https://helloworld-kikbot.herokuapp.com/incoming'
+        webhook=('https://%s/incoming' % host_name)
     )
 )
 
